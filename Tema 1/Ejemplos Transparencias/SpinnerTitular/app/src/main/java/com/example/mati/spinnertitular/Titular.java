00f693ca81ba6467de3677323691c6eb35a00582
XaviNavarro <dam.xnavarro@institutoserpis.org>
2016-10-19 09:20:36 +0200

    Actualizando

---
+++ Tema 1/Ejemplos Transparencias/SpinnerTitular/app/src/main/java/com/example/mati/spinnertitular/Titular.java
@@ -0,0 +1,13 @@
+package com.example.mati.spinnertitular;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Titular extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_titular);
+    }
+}
