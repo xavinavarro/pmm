d80596127dd6508436cc124cd3152a3eb3db1c02
XaviNavarro <dam.xnavarro@institutoserpis.org>
2016-11-09 09:44:10 +0100

    Animacion update

--- Tema 1/EjemploAnimacion/app/src/main/java/com/example/mati/ejemploanimacion/MainActivity.java
+++ Tema 1/EjemploAnimacion/app/src/main/java/com/example/mati/ejemploanimacion/MainActivity.java
@@ -10,25 +10,23 @@ import android.widget.ImageView;
 public class MainActivity extends Activity {
     AnimationDrawable animacion;
 
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Obtenemos el recurso creado en animacion.xml
+
         animacion = (AnimationDrawable) getResources().getDrawable(R.drawable.animacion);
-        //Creamos una vista que contendrá una imagen
+
         ImageView imagen = new ImageView(this);
-        //Le ponemos color de fondo
+
         imagen.setBackgroundColor(Color.WHITE);
-        //Cargamos en lugar de una imagen, una animación.
+
         imagen.setImageDrawable(animacion);
         setContentView(imagen);
     }
 
     public boolean onTouchEvent(MotionEvent evento) {
-        //Al realizar una pulsación en pantalla
+
         if (evento.getAction() == MotionEvent.ACTION_DOWN) {
-            //Ponemos en marcha la animación
             animacion.start();
             return true;
         }
